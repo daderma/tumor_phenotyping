495d6163e59cebe9f2633e25e08ee9560b484a4c
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2016-04-25 16:03:26 -0400

    Change toolset to vs140_xp

--- build-dependencies.cmd
+++ build-dependencies.cmd
@@ -4,7 +4,7 @@ set BOOST_PREFIX=%DEPENDENCIES_PREFIX%\boost
 set ZLIB_PREFIX=%DEPENDENCIES_PREFIX%\zlib
 set LIBTIFF_PREFIX=%DEPENDENCIES_PREFIX%\libtiff
 set INSTALL_PREFIX=%DEPENDENCIES_PREFIX%\install
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 
 if exist "%INSTALL_PREFIX%" (goto done)
@@ -14,27 +14,27 @@ pushd "%BOOST_PREFIX%"
 call bootstrap.bat
 b2 headers
 set BOOST_LIBRARIES=--with-filesystem
-b2 --toolset=msvc-12.0 --prefix="%INSTALL_PREFIX%\debug" --layout=tagged %BOOST_LIBRARIES% address-model=64 link=static runtime-link=shared variant=debug install
-b2 --toolset=msvc-12.0 --prefix="%INSTALL_PREFIX%\release" --layout=tagged %BOOST_LIBRARIES% address-model=64 link=static runtime-link=shared variant=release install
+b2 --toolset=msvc-14.0 --prefix="%INSTALL_PREFIX%\debug" --layout=tagged %BOOST_LIBRARIES% address-model=64 link=static runtime-link=shared variant=debug install
+b2 --toolset=msvc-14.0 --prefix="%INSTALL_PREFIX%\release" --layout=tagged %BOOST_LIBRARIES% address-model=64 link=static runtime-link=shared variant=release install
 popd
 
 
 pushd "%ZLIB_PREFIX%"
 git clean -d -f
-cmake -G "Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=..\install\debug .
+cmake -G "Visual Studio 14 Win64" -DCMAKE_INSTALL_PREFIX=..\install\debug .
 cmake --build . --target install --config debug
 git clean -d -f
-cmake -G "Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=..\install\release .
+cmake -G "Visual Studio 14 Win64" -DCMAKE_INSTALL_PREFIX=..\install\release .
 cmake --build . --target install --config release
 popd
 
 
 pushd "%LIBTIFF_PREFIX%"
 git clean -d -f
-cmake -G "Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=..\install\debug -DBUILD_SHARED_LIBS=OFF .
+cmake -G "Visual Studio 14 Win64" -DCMAKE_INSTALL_PREFIX=..\install\debug -DBUILD_SHARED_LIBS=OFF .
 cmake --build . --target install --config debug
 git clean -d -f
-cmake -G "Visual Studio 12 Win64" -DCMAKE_INSTALL_PREFIX=..\install\release -DBUILD_SHARED_LIBS=OFF .
+cmake -G "Visual Studio 14 Win64" -DCMAKE_INSTALL_PREFIX=..\install\release -DBUILD_SHARED_LIBS=OFF .
 cmake --build . --target install --config release
 popd
 
